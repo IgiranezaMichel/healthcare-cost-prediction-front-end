c2648f77109932b9c8cd53e3f07903efdc98d82f
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-10 00:39:27 +0200

    add equipement

---
+++ src/util/constant/equipment.tsx
@@ -0,0 +1,3 @@
+export const EquipmentCategory=[
+    'Diagnostic','Therapeutic ','Surgical','Medical Supplies','Medical Furniture','Patient Monitoring','Orthopedic Product','Dental Product','Laboratory Equipment','Pharmaceutical','Rehabilitation Equipment','Medical Imaging','Infection Control','Home Healthcare','Emergency Medical'
+]
