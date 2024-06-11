6a393a289b86e24992bd52b4165ffcc8c4144027
IgiranezaMichel <igiramichael@gmail.com>
2024-06-11 19:04:34 +0200

    product detail interface

---
+++ src/interface/productDetail.tsx
@@ -0,0 +1,13 @@
+export interface IProductDetail{
+    id?:string
+    productId?:string
+    productName:string
+    productCategory:string
+    manufacturer:string
+    manufactureDate:string
+    country:string
+    quantity:number
+    unit:string
+    price:number
+    currency:string
+}
